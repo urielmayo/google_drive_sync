c05ed05139d0fd617f1c1b4fcb253f775e689692
urielmayo <urimayo00@gmail.com>
2021-06-24 13:51:31 -0300

    configuring google drive api

---
+++ drive.py
@@ -0,0 +1,11 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth()
+
+drive = GoogleDrive(gauth)
+
+file_list = drive.ListFile({'q': "'root' in parents"}).GetList()
+for file1 in file_list:
+  print('title: %s, id: %s' % (file1['title'], file1['id']))
